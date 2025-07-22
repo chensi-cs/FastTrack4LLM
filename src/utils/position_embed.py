05812552fd79d8f57a8f0a085b0b559195d2e99d
chensi-cs <3206747178@qq.com>
2025-07-22 15:44:28 +0800

    增加RoPE位置编码

--- src/utils/position_embed.py
+++ src/utils/position_embed.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-def Sinusoidal_position_embed(config):
+def Sinusoidal_position_embed(max_len,d_model):
     """
     生成位置编码张量。
     参数:
@@ -13,8 +13,8 @@ def Sinusoidal_position_embed(config):
     返回:
         位置编码张量，形状为 [1, max_len, d_model]
     """
-    d_model = config.d_model
-    max_len = config.max_len
+    d_model = d_model
+    max_len = max_len
     pe = torch.zeros(max_len,d_model)
     # shape : (max_len, 1)
     positon = torch.arange(0,max_len).unsqueeze(1).float()
@@ -27,7 +27,7 @@ def Sinusoidal_position_embed(config):
     pe = pe.unsqueeze(0)
     return pe
 
-def precompute_rope_matrix(config):
+def precompute_rope_matrix(max_len,d_model):
     """
     生成旋转位置编码矩阵
     参数：
@@ -36,25 +36,30 @@ def precompute_rope_matrix(config):
     返回：
         旋转位置编码矩阵
     """
-    d_model = config.d_model
-    max_len = config.max_len
+    d_model = d_model
+    max_len = max_len
     # 频率,决定不同维度的旋转速度 shape: (d_model//2,)
     theta = 10000.0
     freqs = 1.0 / (theta ** (torch.arange(0, d_model, 2).float() / d_model))
-    # 位置,[0, 1, 2, ..., end-1]  shape: (sequence_length, 1)
+    # 位置,[0, 1, 2, ..., max_len-1]  shape: (max_len, 1)
     t = torch.arange(max_len,dtype=torch.float32).unsqueeze(1)
     # 计算位置和频率的外积，得到每个位置在各维度的角度值 freqs[m,i] = t[m] * freqs[i]，表示位置 m 在维度 i的旋转角度
-    # 旋转位置编码矩阵, 计算位置和频率的乘积 [seq_len, d_model//2]
+    # 旋转位置编码矩阵, 计算位置和频率的乘积 [max_len, d_model//2]
     freqs =  torch.outer(t,freqs).float()
     
-    # 计算余弦和正弦值
+    # 计算余弦和正弦值，freqs_sin 和 freqs_cos 是预计算的正弦和余弦矩阵，用于对查询（query）和键（key）向量进行旋转操作。
+    # freqs_sin 和 freqs_cos 具体含义：存储每个位置在不同维度上的旋转角度的正弦和余弦值
     # 这里将结果在最后一维复制，以便后续与向量的偶数和奇数维度匹配
-    # shape: (seq_len, d_model)
+    # shape: (max_len, d_model)
+    # 为什么需要复制？
+
+    # RoPE 将向量的每两个维度视为一个复数对（实部和虚部）。例如，对于维度 i 和 i+1：
+
+    # freqs_sin[m, i] 和 freqs_sin[m, i+1] 存储相同的正弦值。
+    # freqs_cos[m, i] 和 freqs_cos[m, i+1] 存储相同的余弦值。
     freqs_sin = torch.cat([torch.sin(freqs),torch.sin(freqs)],dim=-1)
     freqs_cos = torch.cat([torch.cos(freqs),torch.cos(freqs)],dim=-1)
     return freqs_sin,freqs_cos
-
-
 
 def apply_rotary_pos_emb(q,k,cos,sin,position_ids=None):
     """
